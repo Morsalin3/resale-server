eca42564a326a34f447b2f5acb1d61a77415512d
Morsalin3 <mdmorsalin3@gmail.com>
2022-11-27 02:27:30 +0600

    create brand option route

--- index.js
+++ index.js
@@ -13,8 +13,24 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ojgvcnk.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const categoryCollection = client.db("swap").collection("category");
+
+        app.get('/category', async(req, res)=>{
+            const query = {};
+            const result = await categoryCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
 
 
 
